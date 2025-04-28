5cd720dbb2d1689858d9a42c82f4656553bed128
miyagawananako <miyagawa775@gmail.com>
2025-04-28 15:54:43 +0900

    change to be able to use styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from "react";
-import "./App.css";
+import styled from "styled-components";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
+    <AppContainer>
+      <AppHeader>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
+        <AppLink
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </AppLink>
+      </AppHeader>
+    </AppContainer>
   );
 };
 
 export default App;
+
+const AppContainer = styled.div`
+  text-align: center;
+`;
+
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
+const AppLink = styled.a`
+  color: #61dafb;
+`;
